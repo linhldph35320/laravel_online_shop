59ce057995ae324bf383d5422becda2d016ed864
linhldph35320 <linhldph35320@fpt.edu.vn>
2024-05-06 12:34:54 +0700

    sửa + xóa
    sửa + xóa

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -13,29 +13,32 @@ use Intervention\Image\Drivers\Gd\Driver;
 
 class CategoryController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $categories = Category::latest();
 
-        if(!empty($request->get('keyword'))){
-            $categories = Category::where('name','like','%'.$request->get('keyword').'%');
+        if (!empty($request->get('keyword'))) {
+            $categories = Category::where('name', 'like', '%' . $request->get('keyword') . '%');
         }
 
         $categories = $categories->paginate(10);
 
-        return view('admin.category.list',compact('categories'));
+        return view('admin.category.list', compact('categories'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('admin.category.create');
     }
 
-    public function store(Request $request){
-        $validator = Validator::make($request->all(),[
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'slug' => 'required|unique:categories',
         ]);
 
-        if($validator->passes()){
+        if ($validator->passes()) {
 
             $category = new Category();
             $category->name = $request->name;
@@ -44,40 +47,39 @@ class CategoryController extends Controller
             $category->save();
 
             // Lưu hình ảnh ở đây
-            if(!empty($request->image_id)){
+            if (!empty($request->image_id)) {
                 $tempImage = TempImage::find($request->image_id);
 
-                $extArray = explode('.',$tempImage->name);
+                $extArray = explode('.', $tempImage->name);
 
                 $ext = last($extArray); // hàm last để lấy giá trị cuối trong mảng
 
-                $newImageName = $category->id.'.'.$ext;
+                $newImageName = $category->id . '.' . $ext;
 
-                $sPath = public_path().'/temp/'.$tempImage->name;
-                $dPath = public_path().'/uploads/category/'.$newImageName;
+                $sPath = public_path() . '/temp/' . $tempImage->name;
+                $dPath = public_path() . '/uploads/category/' . $newImageName;
 
-                File::copy($sPath,$dPath);
+                File::copy($sPath, $dPath);
 
                 // Tạo thumb
-                $sourcePath = public_path('/uploads/category/'.$newImageName);
-                $dPath = public_path().'/uploads/category/thumb/'.$newImageName;
+                $sourcePath = public_path('/uploads/category/' . $newImageName);
+                $dPath = public_path() . '/uploads/category/thumb/' . $newImageName;
                 $manager = new ImageManager(new Driver());
                 $image = $manager->read($sourcePath);
-                $image->resize(450,600);
+                $image->cover(450,600);
                 $image->save($dPath);
 
                 $category->image = $newImageName;
                 $category->save();
             }
 
-            session()->flash('success','Category added successfully.');
+            session()->flash('success', 'Category added successfully.');
 
             return response()->json([
                 'status' => true,
                 'message' => 'Category added successfully.'
             ]);
-
-        }else{
+        } else {
             return response()->json([
                 'status' => false,
                 'errors' => $validator->errors()
@@ -85,15 +87,113 @@ class CategoryController extends Controller
         }
     }
 
-    public function edit(){
+    public function edit($categoryId, Request $request)
+    {
+        $category = Category::find($categoryId);
 
+        if (empty($category)) {
+            return redirect()->route('categories.index');
+        }
+
+        return view('admin.category.edit', compact('category'));
     }
 
-    public function update(){
+    public function update($categoryId, Request $request)
+    {
+        $category = Category::find($categoryId);
 
+        if (empty($category)) {
+            session()->flash('error','Category not found');
+            return response()->json([
+                'status' => false,
+                'notFound' => true,
+                'message' => 'Category not found'
+            ]);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,'.$category->id.',id',
+        ]);
+
+        if ($validator->passes()) {
+
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            $oldImage = $category->image;
+
+            // Lưu hình ảnh ở đây
+            if (!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+
+                $extArray = explode('.', $tempImage->name);
+
+                $ext = last($extArray); // hàm last để lấy giá trị cuối trong mảng
+
+                $newImageName = $category->id.'-'.time(). '.' . $ext;
+
+                $sPath = public_path() . '/temp/' . $tempImage->name;
+                $dPath = public_path() . '/uploads/category/' . $newImageName;
+
+                File::copy($sPath, $dPath);
+
+                // Tạo thumb
+                $sourcePath = public_path('/uploads/category/' . $newImageName);
+                $dPath = public_path() . '/uploads/category/thumb/' . $newImageName;
+                $manager = new ImageManager(new Driver());
+                $image = $manager->read($sourcePath);
+                // $image->resize(450, 600);
+                $image->cover(450,600);
+                $image->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+
+                // Xóa ảnh cũ khi update
+                File::delete(public_path().'/uploads/category/thumb/'.$oldImage);
+                File::delete(public_path().'/uploads/category/'.$oldImage);
+            }
+
+            session()->flash('success', 'Category updated successfully.');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Category updated successfully.'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
     }
 
-    public function destroy(){
+    public function destroy($categoryId, Request $request)
+    {
+        $category = Category::find($categoryId);
 
+        if(empty($category)){
+            session()->flash('error','Category not found');
+            return response()->json([
+                'status'=>true,
+                'message' => 'Category not found'
+            ]);
+            // return redirect()->route('categories.index');
+        }
+
+        File::delete(public_path().'/uploads/category/thumb/'.$category->image);
+        File::delete(public_path().'/uploads/category/'.$category->image);
+
+        $category->delete();
+
+        session()->flash('success','Category deleted successfully.');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Category deleted successfully.'
+        ]);
     }
 }
--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -24,11 +24,13 @@
                 <form action="" method="GET">
                     <div class="card-header">
                         <div class="card-title">
-                            <button type="button" onclick="window.location.href='{{ route('categories.index') }}'" class="btn btn-default btn-sm">Reset</button>
+                            <button type="button" onclick="window.location.href='{{ route('categories.index') }}'"
+                                class="btn btn-default btn-sm">Reset</button>
                         </div>
                         <div class="card-tools">
                             <div class="input-group input-group" style="width: 250px;">
-                                <input type="text" value="{{ Request::get('keyword') }}" name="keyword" class="form-control float-right" placeholder="Search">
+                                <input type="text" value="{{ Request::get('keyword') }}" name="keyword"
+                                    class="form-control float-right" placeholder="Search">
 
                                 <div class="input-group-append">
                                     <button type="submit" class="btn btn-default">
@@ -76,7 +78,7 @@
                                             @endif
                                         </td>
                                         <td>
-                                            <a href="#">
+                                            <a href="{{ route('categories.edit', $category->id) }}">
                                                 <svg class="filament-link-icon w-4 h-4 mr-1"
                                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                                     fill="currentColor" aria-hidden="true">
@@ -85,7 +87,8 @@
                                                     </path>
                                                 </svg>
                                             </a>
-                                            <a href="#" class="text-danger w-4 h-4 mr-1">
+                                            <a href="#" onclick="deleteCategory({{ $category->id }})"
+                                                class="text-danger w-4 h-4 mr-1">
                                                 <svg wire:loading.remove.delay="" wire:target=""
                                                     class="filament-link-icon w-4 h-4 mr-1"
                                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -124,4 +127,26 @@
 @endsection
 
 @section('customJs')
+    <script>
+        function deleteCategory(id) {
+            var url = '{{ route('categories.delete', 'ID') }}'
+            var newUrl = url.replace("ID", id)
+            if (confirm("Are you sure you want to delete")) {
+                $.ajax({
+                    url: newUrl,
+                    type: 'delete',
+                    data: {},
+                    dataType: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function(response) {
+                        if (response["status"]) {
+                            window.location.href = "{{ route('categories.index') }}";
+                        }
+                    }
+                });
+            }
+        }
+    </script>
 @endsection
